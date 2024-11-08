677d005fc2f49a791f998931b112e38ab1a93408
Michael Hunter <elec.mike@gmail.com>
2024-11-08 14:50:43 +1100

    initial

---
+++ servo_test.py
@@ -0,0 +1,19 @@
+#Here we will import all the extra functionality desired
+from time import *
+from adafruit_servokit import ServoKit
+
+#Below is an initialising statement stating that we will have access to 16 PWM channels of the HAT and to summon them we will use | kit |
+kit = ServoKit(channels=16)
+
+#Below desides the initial angle that the servo which is attatched to Port 0 will be. In this case we will make it zero degrees.
+kit.servo[0].angle = None
+
+#Below will create an infinite loop
+while True:
+    
+    sleep(1)
+    kit.servo[0].angle = 0
+    sleep(1) 
+    kit.servo[0].angle = 90
+    sleep(1)
+    kit.servo[0].angle = 180
